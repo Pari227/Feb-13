66994121464c988f3b213cec27ce8603ee8974a6
raja-16411 <pachavan.parivendhan@gmail.com>
2023-02-13 18:30:15 +0530

    first-commit

---
+++ src/main/java/my_app_java/new_java/App.java
@@ -0,0 +1,15 @@
+package my_app_java.new_java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Good Job!" );
+        System.out.println( "Good!" );
+    }
+}
