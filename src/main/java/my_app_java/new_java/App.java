f801194cb7609ab6a2000e4567b56bd8c2b0e917
raja-16411 <pachavan.parivendhan@gmail.com>
2023-02-14 10:37:04 +0530

    New modification

--- src/main/java/my_app_java/new_java/App.java
+++ src/main/java/my_app_java/new_java/App.java
@@ -11,6 +11,5 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Good Job!" );
         System.out.println( "Good!" );
-        System.out.println( "Good1234!" );
     }
 }
