941f4298f61476ebe6c50462b5538903eed911fb
raja-16411 <pachavan.parivendhan@gmail.com>
2023-02-16 12:18:23 +0530

    test

--- src/main/java/my_app_java/new_java/App.java
+++ src/main/java/my_app_java/new_java/App.java
@@ -11,6 +11,5 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Good Job!" );
         System.out.println( "123!" );
-        System.out.println( "Testing!" );
     }
 }
